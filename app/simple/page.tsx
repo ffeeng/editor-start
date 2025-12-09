fe6a224b5d893f7911936ba1d5923ad98537e381
feng <949637749@qq.com>
2025-12-09 16:14:03 +0800

    feat: 初始化编辑器

---
+++ app/simple/page.tsx
@@ -0,0 +1,6 @@
+import { SimpleEditor } from "@/components/tiptap-templates/simple/simple-editor"
+import  '@/styles/_variables.scss'
+
+export default function Page() {
+  return <SimpleEditor />
+}
